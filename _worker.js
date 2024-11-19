a993a7dd3a0796fcabfe002b91a8a97ea5181a65
DaBo <165022983+DaBoWin@users.noreply.github.com>
2024-11-19 12:28:11 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -344,7 +344,7 @@ async function fetchMultipleSubscriptions(urls) {
                     decodedResult = base64Decode(result);
                 }
                 const lines = decodedResult.split('\n');
-                return lines.slice(0, 5).join('\n');
+                return base64Encode(lines.slice(0, 5).join('\n'));
             }
             return ''; // 返回空字符串以确保结果数组长度一致
         }));
